7e93720af2fa5da3a35ed17f9e3e9e8434d34eb4
伯大成 <bodacheng123@gmail.com>
2022-07-03 13:31:41 +0900

    try use master node

--- pipeline_script/DevBuild/android/build_custom.groovy
+++ pipeline_script/DevBuild/android/build_custom.groovy
@@ -1,6 +1,6 @@
 pipeline {
     agent {
-        label "DevBuild_JobNode"
+        label "master"
     }
     
     environment {
--- pipeline_script/DevBuild/android/build_period.groovy
+++ pipeline_script/DevBuild/android/build_period.groovy
@@ -1,6 +1,6 @@
 pipeline {
     agent {
-        label "PeriodBuild_JobNode"
+        label "master"
     }
 
     environment {
