4ce6d33ddd6d8dd678e799e9e694b67b3cb5c8a5
伯大成 <bodacheng123@gmail.com>
2022-07-03 21:44:07 +0900

    build printlin

--- pipeline_script/DevBuild/android/build.groovy
+++ pipeline_script/DevBuild/android/build.groovy
@@ -180,6 +180,8 @@ pipeline {
                     commandBuilder.append " -buildKind ${params.BUILD_KIND}"
                     commandBuilder.append " -androidArchitectures '${params.ANDROID_ARCHS}'"
 
+                    println "commandBuilder:" + script:commandBuilder.toString()
+
                     // apk作成
                     sh(script:commandBuilder.toString(), returnStdout:false)
                 }
