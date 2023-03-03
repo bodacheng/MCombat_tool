814a06d67301e1f87ef913681f0e2c6e4c2117a1
伯大成 <bodacheng123@gmail.com>
2023-03-03 16:34:28 +0900

    try

--- pipeline_script/DevBuild/android/build_win.groovy
+++ pipeline_script/DevBuild/android/build_win.groovy
@@ -62,10 +62,10 @@ pipeline {
 //                     '''
 
                     // load git utility
-                    def utilisPath = "${env.WORKSPACE}/pipeline_script/utils"
-                    gitUtility = load "${utilisPath}/gitUtility_win.groovy"
-                    appcenterUtility = load "${utilisPath}/appcenterUtility.groovy"
-
+                    def utilisPath = "${env.WORKSPACE}\\pipeline_script\\utils"
+                    gitUtility = load "${utilisPath}\\gitUtility_win.groovy"
+                    appcenterUtility = load "${utilisPath}\\appcenterUtility.groovy"
+                    println appcenterUtility
 //                     def slackNotifyClass = load "${utilisPath}/notify/SlackNotify.groovy"
 //                     slackNotify = slackNotifyClass.newInstance(env.SLACK_NOTIFY_CHANNEL, "p3-notify-slack-token", params.BUILD_KIND, BUILD_TARGET, "")
 //                     slackUtility = load "${utilisPath}/notify/slackUtility.groovy"
